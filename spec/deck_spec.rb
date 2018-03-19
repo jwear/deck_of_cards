d5e7d03580dcc2f8739fbbb0180f6f1daff594a9
Julie Anne Wear <jag68@icloud.com>
2018-03-18 22:50:57 -0700

    Refactor deck spec

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -2,38 +2,34 @@ require_relative '../lib/card'
 require_relative '../lib/deck'
 
 describe Deck do
+  before do
+   @deck = Deck.new
+  end
 
   it "has 52 cards" do
-    deck = Deck.new
-
-    expect(deck.cards.size).to eq(52)
+    expect(@deck.cards.size).to eq(52)
   end
 
   it "draws a card" do
-    deck = Deck.new
-    card = deck.draw
+    card = @deck.draw
 
-    expect(deck.cards.size).to eq(51)
+    expect(@deck.cards.size).to eq(51)
     expect(card).to be_an_instance_of(Card)
   end
 
   it "draws a card until the deck is empty" do
-    deck = Deck.new
-
     52.times do
-      expect(deck).not_to be_empty
-      deck.draw
+      expect(@deck).not_to be_empty
+      @deck.draw
     end
 
-    expect(deck).to be_empty
+    expect(@deck).to be_empty
   end
 
   it "shuffles the deck" do
-    deck = Deck.new
+    original_cards = @deck.cards.dup
+    @deck.shuffle!
 
-    original_cards = deck.cards.dup
-    deck.shuffle!
-
-    expect(original_cards).not_to eq(deck.cards)
+    expect(original_cards).not_to eq(@deck.cards)
   end
 end
